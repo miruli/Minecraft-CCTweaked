18f6e400484d27591c5a3aa95fbdb87939fa536e
miruli <marcelo.obachi@gmail.com>
2023-10-16 21:23:38 -0300

    functions first

--- utils/clock/clock.lua
+++ utils/clock/clock.lua
@@ -1,13 +1,12 @@
-while true do
-  message = GameTime()
-
-  print(message)
-end
-
 function GameTime()
   return "Day " .. os.day() .. " - " .. textutils.formatTime(os.time())
 end
 
 function RealTime()
   return os.date("%b-%d - %I:%M %p")
+end
+
+while true do
+  message = GameTime()
+  print(message)
 end
