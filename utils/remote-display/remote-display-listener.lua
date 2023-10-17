e2a795cfe7931df543b3cfb48aff8c3471d10588
miruli <marcelo.obachi@gmail.com>
2023-10-16 21:49:26 -0300

    format

--- utils/remote-display/remote-display-listener.lua
+++ utils/remote-display/remote-display-listener.lua
@@ -1,17 +1,15 @@
 --start server
 print("initializing remote display")
-monitor = peripheral.find("monitor") or error("no monitor detected")
+monitor = peripheral.find("monitor") or error("error: no monitor detected")
 monitor.clear()
 
 peripheral.find("modem", rednet.open)
 if (rednet.isOpen()) then
   print("connected, waiting for messages:")
-
   while true do
     id, message = rednet.receive()
     print(("computer %d: %s"):format(id, message))
   end
-
 else
   print("not connected")
 end
